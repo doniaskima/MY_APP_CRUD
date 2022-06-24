8f955272b36e99f32e047d0b46cf8107ade7a508
doniaskima <doniaskima344@gmail.com>
2022-06-24 16:11:32 +0100

    my app

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const bodyparser = require("body-parser");
 const path = require('path');
+const mongoose = require("mongoose");
 
 const connectDB = require('./server/database/connection');
 
@@ -14,8 +15,14 @@ const PORT = process.env.PORT || 8080
 // log requests
 app.use(morgan('tiny'));
 
-// mongodb connection
-connectDB();
+//DB connection
+mongoose.connect("mongodb://localhost:27017/MY_app_CRUD");
+mongoose.connection.on("connected", () => {
+    console.log("DB connected");
+});
+mongoose.connection.on("error", (err) => {
+    console.log("mongodb failed with", err);
+});
 
 // parse request to body-parser
 app.use(bodyparser.urlencoded({ extended: true }))
@@ -31,5 +38,8 @@ app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 
 // load routers
 app.use('/', require('./server/routes/router'))
+const port = 8000;
 
-app.listen(PORT, () => { console.log(`Server is running on http://localhost:${PORT}`) });
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
